80ee1fab9f4908f13c3f1b9db98de76c89e693ad
novamaster00 <109971027+novamaster00@users.noreply.github.com>
2025-07-12 23:16:10 +0530

    Revert "CORS fix for backend to get reqest from frontend"
    This reverts commit 59404a71bdbb24cf856088813448fc1c92a33441.

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
 
     'http://localhost:5173',
     'http://localhost:8080',
-    'https://salon-managment.vercel.app'
+    'https://salon-managment.vercel.app/'
 ]
 
 module.exports = allowedOrigins
