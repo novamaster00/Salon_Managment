5d1509f6923b71858bbdbd38f4fda1e76fcee7f0
novamaster00 <109971027+novamaster00@users.noreply.github.com>
2025-07-27 00:12:29 +0530

    CORS problem solve

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
 
     'http://localhost:5173',
     'http://localhost:8080',
-    'https://salon-managment.vercel.app'
+    'https://dk-hairsalon-z3gk.onrender.com/'
 ]
 
 module.exports = allowedOrigins
